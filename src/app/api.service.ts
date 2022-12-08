0e573dc8c55df68dd5b5a7b0c222d594bbd6ca19
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-08 23:09:07 +0530

    service

---
+++ src/app/api.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+
+
+  constructor(private http:HttpClient) { }
+  addbook=(datatosend:any)=>
+  {
+    return this.http.post("http://localhost:8080/add",datatosend)
+  }
+  viewbook =()=>
+  {
+    return this.http.get("http://localhost:8080/view")
+  }
+  
+  readValue=(datatosend:any)=>
+  {
+    return this.http.post("http://localhost:8080/search",datatosend)
+  }
+  deletestudent=(datatosend:any)=>
+  {
+    return this.http.post("http://localhost:8080/delete",datatosend)
+  }
+}
