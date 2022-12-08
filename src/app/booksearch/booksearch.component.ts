5141184feb6ba6a3e0a02dfd88a9620808443e63
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-08 23:28:43 +0530

    delete from db

--- src/app/booksearch/booksearch.component.ts
+++ src/app/booksearch/booksearch.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-booksearch',
@@ -7,12 +9,57 @@ import { Component } from '@angular/core';
 })
 export class BooksearchComponent {
 
-  title=""
 
-  searchValues=()=>
+  constructor(private api:ApiService,private router:Router) {}
+  booktitle=""
+  sdata:any=[]
+readValue=()=>
+{
+  let data =
   {
-  let data:any=
-  {"title":this.title}
-  console.log(data)
+    "booktitle":this.booktitle
   }
+  console.log(data)
+  this.api.readValue(data).subscribe(
+    (response:any)=>
+    {
+      console.log(response)
+      if(response.length==0)
+      {
+         alert("no data")
+      }
+      else {
+
+        this.sdata=response
+
+      }
+    }
+  )
+}
+
+deleteBtnClick=(id:any)=>
+{
+  let data:any={
+    "id":id
+  }
+    this.api.deletestudent(data).subscribe(
+      (generated:any)=>
+      {
+        console.log(generated)
+        if(generated.status == "success")
+        {
+          alert("Book Deleted")
+          this.router.navigate(["/view"])
+        }
+        else{
+          alert("error")
+        }
+      }
+    )
+    
+     
+         
+}
+
+
 }
